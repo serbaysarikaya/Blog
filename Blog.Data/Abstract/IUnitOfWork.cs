2e35a7f645cc949497a3b10a2b206208a7882b9f
serbaysarikaya <39198551+serbaysarikaya@users.noreply.github.com>
2023-01-22 14:34:23 +0300

    xd

---
+++ Blog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IArticleRepository Articles { get; } 
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+        
+        Task<int> SaveAsync();
+
+    }
+}
